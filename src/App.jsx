8ed632d5b24c859dbfad0541350c3eff1d937483
gozneokhan <gozneokhan@gmail.com>
2024-05-03 00:09:12 +0900

    feat: simple-counter-app-ui-implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
+import Viewer from './components/Viewer';
+import Controller from './components/Controller';
 
 function App() {
-    return <div></div>;
+    return (
+        <div className="APP">
+            <h1>Simple Counter</h1>
+            <section>
+                <Viewer />
+            </section>
+            <section>
+                <Controller />
+            </section>
+        </div>
+    );
 }
 
 export default App;
